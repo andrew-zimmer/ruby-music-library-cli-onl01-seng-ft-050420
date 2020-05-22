394c821b24073ffe75246269f0b5119c6560f951
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-22 13:41:04 +0000

    Automatically backed up by Learn

--- lib/music_lib_controller.rb
+++ lib/music_lib_controller.rb
@@ -24,4 +24,8 @@ class MusicLibraryController
   def list_songs 
     Song.all.sort{|a,b| a.name <=> b.name}.each.with_index(1){|song, index| puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end 
+  
+  def list_artists
+    Artist.all.sort{|a,b| a.name <=> b.name}.each.with_index(1){|artist, index| puts "#{index}. #{artist}"}
+  end 
 end 
