e7e1be416b487d1b2a1eb4f7662227ee929d6110
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-22 13:13:19 +0000

    Automatically backed up by Learn

--- lib/music_lib_controller.rb
+++ lib/music_lib_controller.rb
@@ -18,7 +18,7 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     respond = gets.strip
-    until respond = "exit"
+    while respond != "exit"
       call 
     end 
     binding.pry
